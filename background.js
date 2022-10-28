d85d6ccc7ed9c6277307010fde39579b192015dd
Khaue <khaueramos123@gmail.com>
2022-10-28 15:36:02 -0300

    1.0

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel:true }},
+    { urls: ["*://*.zedo.com/*"] },
+    ["blocking"]
+)
+
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
